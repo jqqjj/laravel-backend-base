14e520e4df4411dd779bb83ad7e0edee8ad08007
LiuHL <78560127@qq.com>
2017-10-17 10:22:13 +0800

    add migration

---
+++ src/database/migrations/2017_10_17_015848_create_table_human_auth.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableHumanAuth extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('handshake', function (Blueprint $table) {
+            $table->char('handshake_id',32);
+            $table->integer('remaining')->unsigned()->default(0);
+            $table->timestamp('expired_time')->nullable();
+            $table->primary('handshake_id');
+        });
+        Schema::create('attempt', function (Blueprint $table) {
+            $table->char('handshake_id',32);
+            $table->tinyInteger('status')->unsigned()->default(0);
+            $table->timestamp('add_time')->nullable();
+            $table->char('ip')->default("");
+            $table->index('handshake_id');
+            $table->index('status');
+            $table->index('add_time');
+            $table->index('ip');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('handshake');
+        Schema::dropIfExists('attempt');
+    }
+}
